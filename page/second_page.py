46d5b61364e60c4294231d3a3cfac3516ccdfe85
JIHYEON KWON <rnjswlgus98@gmail.com>
2023-09-21 17:46:44 +0900

    3조 프로젝트 푸바오, 호랑이 그래프

--- page/second_page.py
+++ page/second_page.py
@@ -1,9 +1,14 @@
 import streamlit as st
-
+from PIL import Image
+from utils import zoo_donut_chart as zoo
+from utils import trend_fubao as tf
+from utils import youtube_vs_graph as yvg
 
 class project:
     def __init__(self):
         self.subheader = '조별 프로젝트'
+        self.image1_path = 'page\\img\\fubao_news.png'
+        self.image1 = Image.open(self.image1_path)
 
     def app2(self):
         st.subheader(self.subheader)
@@ -11,9 +16,60 @@ class project:
         with tab1:
             st.write('방문객 변화')
         with tab2:
-            st.write('푸바오의 화제성')
+            st.write(f"**1. 에버랜드 판다 푸바오의 인기?**")
+            st.image(self.image1)
+            st.info('''
+            푸바오의 인기는 그야말로 폭발적입니다. 국내 최초 자연번식으로 태어난 판다로서, 그의 독특한 매력은 많은 이들의 마음을 사로잡았습니다.
+            푸바오가 최근 TV 프로그램에서 돌보는 사육사와의 따뜻한 이야기가 알려지면서, 그의 매력은 한층 더 돋보이게 되었습니다. 
+            인기에 힘입어 에버랜드 판다 푸바오의 다양한 굿즈들이 판매되었고, 해당 상품이 오픈된 직후 주문 폭주로 인해 홈페이지가 일시적으로 다운되는 사태가 발생하기도 하였습니다.
+            ''')
+            st.write(' ')
+            st.write(' ')
+
+            st.write(f"**2. 유튜브 검색량으로 알아보는 푸바오의 인기**")
+            trend_fubao = tf.TrendFubao("trend_fubao.csv")
+            fubao_graph = trend_fubao.plot_monthly_search_volume()
+            st.pyplot(fubao_graph)
+            st.info('''
+            올해 초, 우리는 긴 지친 코로나의 그림자로부터 벗어나며 새로운 희망과 함께 봄을 맞이했습니다. 그동안의 거리두기와 마스크 착용의 일상이 조금씩 누그러지자, 우리의 시선은 푸바오에게로 향했습니다.
+            푸바오, 그 귀여운 판다는 이제까지 우리의 마음을 사로잡고 있었습니다. 그러나 4월부터, 푸바오의 존재와 이야기는 한층 더 주목받기 시작했습니다. 특히, 한 TV 프로그램을 통해 푸바오의 사육사와의 따뜻한 인연이 소개되면서, 그의 인기는 폭발적으로 증가했습니다.
+            4월부터 차츰 푸바오의 검색량이 증가하더니 5월부터는 폭발적으로 증가하고 있는 추세입니다.
+            ''')
+            st.write(' ')
+            st.write(' ')
+            
+            st.write(f"**3. 에버랜드 동물원 유튜브 채널 '말하는동물원 뿌빠TV'의 최근 50개 동영상 비율로 알아보는 푸바오 인기**")
+            api_key = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
+            channel_analyzer = zoo.YouTubeChannelAnalyzer(api_key)
+            channel_name = '말하는동물원 뿌빠TV'
+            channel_id = channel_analyzer.get_channel_info(channel_name)
+
+            if channel_id:
+                video_titles, video_ids = channel_analyzer.get_channel_videos(channel_id)
+                video_details = channel_analyzer.get_video_details(video_ids)
+                fig = channel_analyzer.plot_channel_analysis(video_details)
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot(fig)
+            else:
+                st.write("채널 정보를 찾을 수 없습니다.")
+            st.info('''
+            에버랜드는 푸바오를 효과적으로 마케팅 활용하고 있음을 확인할 수 있습니다. '말하는동물원 뿌빠TV'의 영상들은 에버랜드의 동물원과 관련된 컨텐츠로, 푸바오와 사육사분들의 모습을 다양하게 보여주고 있습니다. 푸바오가 포함된 영상은 전체의 과반수 이상을 차지하고 있습니다.
+            더불어, 푸바오를 포함한 동영상의 조회수가 전체 조회수의 80% 이상을 차지하고 있으므로, 이 컨텐츠가 관람객들에게 높은 관심을 불러일으키고 있음을 알 수 있습니다. 이는 푸바오를 특별한 캐릭터로 활용하고 있는 결과로 보입니다.
+            에버랜드가 푸바오를 성공적으로 마케팅하고 있다는 것은 동물원의 인기를 높이고, 더불어 에버랜드 방문객들에게 더 많은 관심을 끌고 있다는 것을 시사합니다. 푸바오의 매력을 잘 포착하여 다양한 콘텐츠를 제공함으로써, 에버랜드는 관람객들의 기대와 호기심을 충족시키며 성공적인 마케팅 전략을 시행하고 있는 것으로 평가됩니다.
+            ''')
         with tab3:
-            st.write('한국 호랑이 관심도 증가에 따른 방문객 변화')
+            st.write(f'**1. 푸바오와 호랑이(태범이, 무궁이) 유튜브 조회수 비교**')
+            api_key = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
+            query1 = '푸바오'
+            query2 = '태범이 무궁이'
+            vs_graph = yvg.YouTubeAPI.plot_top_10_videos(api_key, query1, query2, max_results=10)
+            st.pyplot(vs_graph)
+            st.info('''
+            에버랜드의 두 마스코트, 판다 푸바오와 호랑이 태범이 무궁이 유튜브에서의 조회수 경쟁은 흥미로운 결과를 보여주고 있습니다. 2020년에 공개된 호랑이 태범이, 무궁이의 영상이 가장 높은 조회수를 기록했으며, 그 인기는 매우 두드러졌습니다.
+            그러나 흥미로운 점은 2위부터는 모두 최근에 올라온 푸바오의 영상들이 차지하고 있다는 것입니다. 푸바오의 사랑스러운 모습과 독특한 매력은 유튜브 사용자들 사이에서 큰 인기를 얻고 있으며, 그 결과로 그의 영상들은 높은 조회수를 기록하고 있습니다.
+            이러한 결과를 통해 푸바오가 얼마나 많은 사람들에게 사랑받고 있는지, 푸바오의 매력이 얼마나 많은 이들을 매료시키고 있는지를 확인할 수 있습니다. 
+            ''')
+            
         with tab4:
             st.write('동물 마케팅 성공에 따른 에버랜드 매출량 변화')
 
---
+++ utils/youtube_vs_graph.py
@@ -0,0 +1,106 @@
+from googleapiclient.discovery import build
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class YouTubeAPI:
+    # YouTubeAPI 클래스의 생성자
+    def __init__(self, api_key):
+        self.DEVELOPER_KEY = api_key
+        self.YOUTUBE_API_SERVICE_NAME = 'youtube'
+        self.YOUTUBE_API_VERSION = 'v3'
+        self.youtube = build(
+            self.YOUTUBE_API_SERVICE_NAME,
+            self.YOUTUBE_API_VERSION,
+            developerKey=self.DEVELOPER_KEY
+        )
+
+    # YouTube에서 비디오를 검색하고 비디오 ID를 반환
+    def search_videos(self, query, max_results=50):
+        search_response = self.youtube.search().list(
+            q=query,
+            order='viewCount',
+            part='snippet',
+            maxResults=max_results
+        ).execute()
+        video_ids = [item['id']['videoId'] for item in search_response['items']]
+        return video_ids
+
+    # 비디오 ID 목록에 해당하는 비디오의 상세 정보를 반환
+    def get_video_details(self, query, max_results=50):
+        video_ids = self.search_videos(query, max_results)
+        titles = []
+        dates = []
+        views = []
+        #comments = []
+
+        for video_id in video_ids:
+            request = self.youtube.videos().list(
+                id=video_id,
+                part='snippet,contentDetails,statistics'
+            )
+
+            response = request.execute()
+
+            if response['items'] == []:
+                titles.append('-')
+                dates.append('-')
+                views.append('-')
+                #comments.append('-')
+            else:
+                titles.append(response['items'][0]['snippet']['title'])
+                dates.append(response['items'][0]['snippet']['publishedAt'].split('T')[0])
+                views.append(response['items'][0]['statistics']['viewCount'])
+                #comments.append(response['items'][0]['statistics']['commentCount'])
+
+        detail_df = pd.DataFrame([titles, dates, views]).T
+        detail_df.index = detail_df.index + 1
+        detail_df.columns = ['title', 'date', 'view']
+        return detail_df
+
+    # Top 10개 그래프
+    def plot_top_10_videos(api_key, query1, query2, max_results=50):
+        # YouTubeAPI 클래스 인스턴스 생성
+        youtube_api = YouTubeAPI(api_key)
+
+        # 비디오 상세 정보 가져오기
+        video_details1 = youtube_api.get_video_details(query1, max_results=max_results)
+        video_details2 = youtube_api.get_video_details(query2, max_results=max_results)
+
+        # 데이터 정제
+        top_10_videos1 = video_details1.head(10).copy()
+        top_10_videos1['view'] = top_10_videos1['view'].str.replace(',', '').astype(int)
+        top_10_videos1['순위'] = range(1, 11)
+
+        top_10_videos2 = video_details2.head(10).copy()
+        top_10_videos2['view'] = top_10_videos2['view'].str.replace(',', '').astype(int)
+        top_10_videos2['순위'] = range(1, 11)
+
+        # 그래프 그리기
+        plt.rcParams['font.family'] = 'Malgun Gothic'
+        plt.rcParams['axes.unicode_minus'] = False
+
+        plt.figure(figsize=(12, 6))
+        plt.bar(top_10_videos1['순위'] - 0.2, top_10_videos1['view'], width=0.4, color='blue', label=query1)
+        plt.bar(top_10_videos2['순위'] + 0.2, top_10_videos2['view'], width=0.4, color='red', label=query2)
+
+        plt.xticks(top_10_videos1['순위'])  # x 축 라벨을 순위로 설정
+        plt.xlabel('순위')
+        plt.ylabel('조회수')
+        plt.title(f'Top 10 조회수 비교({query1} vs {query2})')
+        plt.legend()
+        plt.show()
+'''
+# 출력
+if __name__ == "__main__":
+    api_key = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
+    youtube_api = YouTubeAPI(api_key)
+    query = '푸바오'
+    video_details = youtube_api.get_video_details(query, max_results=50)
+    print(video_details)
+'''
+
+if __name__ == "__main__":
+    api_key = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
+    query1 = '푸바오'
+    query2 = '태범이 무궁이'
+    YouTubeAPI.plot_top_10_videos(api_key, query1, query2, max_results=10)
