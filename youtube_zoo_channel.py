f50714fbf45dfb070535efa943f2c7c5948f8297
JIHYEON KWON <rnjswlgus98@gmail.com>
2023-09-21 11:32:24 +0900

    유튜브 '말하는뿌빠TV' 푸바오 업로드 컨텐츠 및 조회수 비율(Class)

--- youtube_zoo_channel.py
+++ youtube_zoo_channel.py
@@ -2,118 +2,147 @@ from googleapiclient.discovery import build
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 유튜브 API 연동 및 '말하는동물원 뿌빠TV' 검색
-DEVELOPER_KEY = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
-YOUTUBE_API__SERVICE_NAME = 'youtube'
-YOUTUBE_API_VERSION = 'v3'
+class YouTubeChannelAnalyzer:
+    def __init__(self, api_key):
+        self.DEVELOPER_KEY = api_key
+        self.YOUTUBE_API_SERVICE_NAME = 'youtube'
+        self.YOUTUBE_API_VERSION = 'v3'
+        self.youtube = build(
+            self.YOUTUBE_API_SERVICE_NAME,
+            self.YOUTUBE_API_VERSION,
+            developerKey=self.DEVELOPER_KEY
+        )
 
-youtube = build(YOUTUBE_API__SERVICE_NAME, YOUTUBE_API_VERSION, developerKey = DEVELOPER_KEY)
+    # 채널 정보 가져오기
+    def get_channel_info(self, channel_name):
+        search_query = channel_name
+        zoo_channel = self.youtube.search().list(
+            q=search_query,
+            order='relevance',
+            part='snippet',
+            maxResults=1  # 채널 정보는 하나만 필요하므로 maxResults를 1로 설정
+        ).execute()
 
-search_query = '말하는동물원 뿌빠TV'
+        if 'items' in zoo_channel and zoo_channel['items']:
+            channel_id = zoo_channel['items'][0]['snippet']['channelId']
+        else:
+            print("채널 정보를 찾을 수 없습니다.")
+            return None
 
-zoo_channel = youtube.search().list(
-    q = search_query,
-    order = 'relevance', # 정확도순
-    part = 'snippet',
-    maxResults = 50
-).execute()
+        return channel_id
 
-# 채널 ID 추출
-channel_id = zoo_channel['items'][0]['id']['channelId']
+    # 채널의 동영상 목록 가져오기
+    def get_channel_videos(self, channel_id, max_results=50):
+        if not channel_id:
+            return None
 
-# 채널의 동영상 목록을 가져오기
-request = youtube.search().list(
-    part="snippet",
-    channelId=channel_id,
-    order="date",  # 최신순으로 정렬
-    maxResults=50,
-)
+        request = self.youtube.search().list(
+            part="snippet",
+            channelId=channel_id,
+            order="date",
+            maxResults=max_results,
+        )
 
-response = request.execute()
+        response = request.execute()
 
-video_title_zoo = []
-video_id_zoo = []
+        video_titles = []
+        video_ids = []
 
-for item in response["items"]:
-    video_title = item["snippet"]["title"]
-    video_id = item["id"]["videoId"]
-    video_title_zoo.append(video_title)
-    video_id_zoo.append(video_id)
+        for item in response["items"]:
+            video_title = item["snippet"]["title"]
+            video_id = item["id"]["videoId"]
+            video_titles.append(video_title)
+            video_ids.append(video_id)
 
-df = pd.DataFrame({"Video Title": video_title_zoo, "Video ID": video_id_zoo})
-df.index = df.index + 1
+        return video_titles, video_ids
 
-# 말하는동물원 뿌빠TV 동영상의 상세 정보 추출
-titles = []         # 동영상 제목
-ids = []            # 동영상 id
-dates = []          # 동영상 업로드 날짜
-views = []          # 조회 수
-likes = []          # 좋아요 수
-comments = []       # 댓글 수
+    # 비디오 상세 정보 가져오기
+    def get_video_details(self, video_ids):
+        if not video_ids:
+            return None
 
+        titles = []
+        dates = []
+        views = []
+        likes = []
+        comments = []
 
-for i in range(len(video_id_zoo)):
-    request = youtube.videos().list(
-        id = video_id_zoo[i], # 동영상 id를 입력합니다
-        part = 'snippet,contentDetails,statistics'
-    )
+        for video_id in video_ids:
+            request = self.youtube.videos().list(
+                id=video_id,
+                part='snippet,contentDetails,statistics'
+            )
 
-    response = request.execute()
+            response = request.execute()
 
-    if response['items'] == []: # 동영상 정보가 없을 경우 '-'로 입력
-        titles.append('-')
-        ids.append('-')
-        dates.append('-')
-        views.append('-')
-        likes.append('-')
-        comments.append('-')
+            if response['items'] == []:
+                titles.append('-')
+                dates.append('-')
+                views.append('-')
+                likes.append('-')
+                comments.append('-')
+            else:
+                titles.append(response['items'][0]['snippet']['title'])
+                dates.append(response['items'][0]['snippet']['publishedAt'].split('T')[0])
+                views.append(response['items'][0]['statistics']['viewCount'])
+                likes.append(response['items'][0]['statistics']['likeCount'])
+                comments.append(response['items'][0]['statistics']['commentCount'])
 
-    else:
-        titles.append(response['items'][0]['snippet']['title'])
-        ids.append(video_id_zoo[i])
-        dates.append(response['items'][0]['snippet']['publishedAt'].split('T')[0])
-        views.append(response['items'][0]['statistics']['viewCount'])
-        likes.append(response['items'][0]['statistics']['likeCount'])
-        comments.append(response['items'][0]['statistics']['commentCount'])
+        detail_df = pd.DataFrame([titles, dates, views, likes, comments]).T
+        detail_df.index = detail_df.index + 1
+        detail_df.columns = ['title', 'date', 'view', 'like', 'comment']
 
-detail_df_zoo = pd.DataFrame([titles,dates,views,likes,comments]).T
-detail_df_zoo.index = detail_df_zoo.index+1
-detail_df_zoo.columns = ['title','date','view','like','comment']
+        return detail_df
 
-# 출력
-print(detail_df_zoo)
+    # 채널 분석 결과 시각화
+    def plot_channel_analysis(self, detail_df):
+        plt.rcParams['font.family'] = 'Malgun Gothic'
+        plt.rcParams['axes.unicode_minus'] = False
 
-contains_bao = len(detail_df_zoo[detail_df_zoo['title'].str.contains('바오')])
-not_contains_bao = len(detail_df_zoo) - contains_bao
+        contains_bao = len(detail_df[detail_df['title'].str.contains('바오')])
+        not_contains_bao = len(detail_df) - contains_bao
 
-data = [contains_bao, not_contains_bao]
-labels = ['bao', 'not bao']
-colors = ['#ff9999','#66b3ff']
-explode = (0.1, 0)  # 도넛 차트처럼 구멍을 뚫기 위해 explode 값을 설정합니다.
+        data = [contains_bao, not_contains_bao]
+        labels = ['바오포함', '바오 미포함']
+        colors = ['#ff9999', '#66b3ff']
+        explode = (0.1, 0)
 
-# 첫 번째 도넛 차트 그리기
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.pie(data, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-plt.axis('equal')
-plt.title('bao contain ratio')
+        # 첫 번째 도넛 차트 그리기
+        plt.figure(figsize=(10, 5))
+        plt.subplot(1, 2, 1)
+        plt.pie(data, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+        plt.axis('equal')
+        plt.title("'바오'가 포함된 영상 갯수 비율")
 
-# 'view' 열을 int로 변환
-detail_df_zoo['view'] = detail_df_zoo['view'].astype(int)
+        # 'view' 열을 int로 변환
+        detail_df['view'] = detail_df['view'].astype(int)
 
-bao_views = detail_df_zoo[detail_df_zoo['title'].str.contains('바오')]['view'].sum()
-not_bao_views = detail_df_zoo[~detail_df_zoo['title'].str.contains('바오')]['view'].sum()
+        bao_views = detail_df[detail_df['title'].str.contains('바오')]['view'].sum()
+        not_bao_views = detail_df[~detail_df['title'].str.contains('바오')]['view'].sum()
 
-data = [bao_views, not_bao_views]
-labels = ['bao', 'not bao']
-colors = ['#ff9999','#66b3ff']
-explode = (0.1, 0)
+        data = [bao_views, not_bao_views]
+        labels = ['바오포함', '바오 미포함']
+        colors = ['#ff9999', '#66b3ff']
+        explode = (0.1, 0)
 
-# 두 번째 도넛 차트 그리기
-plt.subplot(1, 2, 2)
-plt.pie(data, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-plt.axis('equal')
-plt.title('bao view comparison')
+        # 두 번째 도넛 차트 그리기
+        plt.subplot(1, 2, 2)
+        plt.pie(data, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+        plt.axis('equal')
+        plt.title("'바오'가 포함된 유튜브 조회수 비교")
 
-plt.tight_layout()
-plt.show()
+        plt.tight_layout()
+        plt.show()
+
+# 사용 예시
+if __name__ == "__main__":
+    api_key = 'AIzaSyCuvYdPw-Lf913DaDTgyEpUiCIjbRdv_yk'
+    channel_analyzer = YouTubeChannelAnalyzer(api_key)
+    channel_name = '말하는동물원 뿌빠TV'
+    channel_id = channel_analyzer.get_channel_info(channel_name)
+
+    if channel_id:
+        video_titles, video_ids = channel_analyzer.get_channel_videos(channel_id)
+        video_details = channel_analyzer.get_video_details(video_ids)
+        print(video_details)
+        channel_analyzer.plot_channel_analysis(video_details)
